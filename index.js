295000f281a92b7bbdb3feeb6ad6c8f31ad068a8
huaerss <969742563@qq.com>
2024-11-17 11:10:04 +0800

    完善功能

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+#!/usr/bin/env node
+
 const { execSync } = require('child_process');
 const { default: inquirer } = require('inquirer');
 const registries = {
     'npm官方': 'https://registry.npmjs.org/',
     '淘宝镜像': 'https://registry.npmmirror.com/',
+    "腾讯镜像": "https://mirrors.cloud.tencent.com/npm/",
+    "华为镜像": "https://mirrors.huaweicloud.com/repository/npm/",
   };
 function getCurrentRegistry() {
     try {
@@ -14,22 +18,29 @@ function getCurrentRegistry() {
   }
 
 
-async function  main() {
-    const currentRegistry = getCurrentRegistry();
-    console.log('当前的镜像源为:', currentRegistry ,'当前的镜像名称为:', Object.keys(registries).find(key => registries[key] === currentRegistry));
-      console.log(Object.keys(registries))
-    const registry = await inquirer.prompt([
-        {
-            type: 'list',
-            name: 'registry',
-            message: '请选择镜像源:',
-            choices: Object.keys(registries),
-        },
-    ]);
-   const registryUrl = registries[registry.registry]
-
-   execSync(`npm config set registry ${registryUrl}`)
-   console.log(`已将镜像源设置为: ${registryUrl}`)
+  async function main() {
+    try {
+        const currentRegistry = getCurrentRegistry();
+        console.log('当前的镜像源为:', currentRegistry ,'当前的镜像名称为:', Object.keys(registries).find(key => registries[key] === currentRegistry));
+        const registry = await inquirer.prompt([
+            {
+                type: 'list',
+                name: 'registry',
+                message: '请选择镜像源:',
+                choices: Object.keys(registries),
+            },
+        ]);
+        const registryUrl = registries[registry.registry]
+        execSync(`npm config set registry ${registryUrl}`)
+        console.log(`已将镜像源设置为: ${registryUrl}`)
+    } catch (error) {
+        if (error.message.includes('User force closed the prompt')) {
+            // console.log('\n程序已被用户终止');
+            process.exit(0); // 正常退出程序
+        } else {
+            console.error('发生错误:', error.message);
+            process.exit(1); // 发生错误时退出
+        }
+    }
 } 
-
 main()
---
+++ readme.md
@@ -0,0 +1,21 @@
+# 快速切换npm镜像源工具 内置淘宝镜像
+
+## 安装
+
+```bash
+pnpm install -g sreg
+
+```
+
+## 使用
+
+```bash
+reg
+```
+
+## 打包
+
+```bash
+pnpm build:mac # 打包mac版本
+pnpm build:win # 打包win版本
+```
